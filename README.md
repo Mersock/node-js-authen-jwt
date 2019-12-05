447593dd2dd0961970c75337cb74d5bd3caad74e
Phumthawan <knzphumthawan@gmail.com>
2019-12-05 17:33:20 +0700

    authen with key rsa file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # node-js-authen-jwt
+
+## Generate RSA Key
+https://www.csfieldguide.org.nz/en/interactives/rsa-key-generator/
+http://travistidwell.com/jsencrypt/demo/
--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
+const fs = require('fs')
 const randtoken = require('rand-token')
-const passport = require('passport');
+const passport = require('passport')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 
-const SECRET = 'ASDFUUSFASFSADFSFWEFMLIUIOPIOI'
+// const SECRET = 'ASDFUUSFASFSADFSFWEFMLIUIOPIOI'
+const privateKEY = fs.readFileSync('./private.key', 'utf8');
+const publicKEY = fs.readFileSync('./public.key', 'utf8');
+
 const refreshTokens = {}
 const opts = {}
 
@@ -18,9 +22,9 @@ app.use(passport.initialize())
 
 
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
-opts.secretOrKey = SECRET;
+opts.secretOrKey = privateKEY;
 
-passport.use(new JwtStrategy(opts,(jwtPayload, done) => {
+passport.use(new JwtStrategy(opts, (jwtPayload, done) => {
     //If the token has expiration, raise unauthorized
     var expirationDate = new Date(jwtPayload.exp * 1000)
     if (expirationDate < new Date()) {
@@ -30,19 +34,35 @@ passport.use(new JwtStrategy(opts,(jwtPayload, done) => {
     done(null, user)
 }))
 
+// issuer — Software organization who issues the token.
+// subject — Intended user of the token.
+// audience — Basically identity of the intended recipient of the token..
+// expiresIn — Expiration time after which the token will be invalid.
+// algorithm — Encryption algorithm to be used to protect the token.
+const signOptions = {
+    issuer: 'Mysoft corp',
+    subject: 'some@user.com',
+    audience: 'http://mysoftcorp.in',
+    expiresIn: 300,
+    algorithm: "RS256"
+};
+
 app.get('/', (req, res) => {
     res.send('ok')
 })
 
 app.post('/login', (req, res, next) => {
     const { username, password } = req.body
-    const user = {
+
+    const payload = {
         username,
         role: 'admin'
     }
-    const token = jwt.sign(user, SECRET, { expiresIn: 300 })
+
+    const token = jwt.sign(payload, privateKEY, signOptions)
     const refreshToken = randtoken.uid(256)
     refreshTokens[refreshToken] = username
+
     res.json({
         token: 'JWT ' + token,
         refreshToken: refreshToken
@@ -50,13 +70,24 @@ app.post('/login', (req, res, next) => {
 })
 
 app.post('/token', function (req, res, next) {
+
+    // if (!req.header('Authorization')) {
+    //     return res.send(401)
+    // }
+    // const authenHeader = req.header('Authorization')
+    // const prefixAuthen = authenHeader.split(" ")
+    // const token = prefixAuthen[1]
+    // const legit = jwt.verify(token, publicKEY, signOptions);
+    // console.log(legit)
+    // console.log("\nJWT verification result: " + JSON.stringify(legit));
+
     const { username, refreshToken } = req.body
-    if ((refreshToken in refreshTokens) && (refreshTokens[refreshToken] == username)) {
-        const user = {
+    if ((refreshToken in refreshTokens) && (refreshTokens[refreshToken] == username) && authenHeader) {
+        const payload = {
             'username': username,
             'role': 'admin'
         }
-        const token = jwt.sign(user, SECRET, { expiresIn: 300 })
+        const token = jwt.sign(payload, privateKEY, signOptions)
         res.json({ token: 'JWT ' + token })
     }
     else {
@@ -72,7 +103,7 @@ app.post('/token/reject', function (req, res, next) {
     res.send(204)
 })
 
-app.get('/test_jwt', passport.authenticate('jwt',{session:false}), (req, res) => {
+app.get('/test_jwt', passport.authenticate('jwt', { session: false }), (req, res) => {
     res.json({ success: 'You are authenticated with JWT!', user: req.user })
 })
 
